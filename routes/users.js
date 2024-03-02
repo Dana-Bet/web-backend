dc451d2efb81192dcc2733588cce8ba62e856be5
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2024-03-02 22:24:16 +0200

    update

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // Public route: Login does not require authentication
 router.post("/login", getUser);
-
+ 
 // Public route: Creating a new user does not require authentication
 router.post("/", createUser);
 
